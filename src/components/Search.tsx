18d70199b82ccd74f834e4004cffe27e94e9fcac
codebudy5247 <a.shekhar5247@gmail.com>
2023-12-21 22:31:14 +0530

    select date in search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { Icon } from "@iconify/react";
 import Select from "react-select";
-import { _city } from "../data/_city";
+import { DateRange, Range } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { format } from "date-fns";
+
 const options = [
   { value: "Basic_room", label: "Basic Room" },
   { value: "luxury_room", label: "Luxury Room" },
@@ -8,37 +13,47 @@ const options = [
 ];
 
 const Search = () => {
+  const [dates, setDates] = useState([
+    {
+      startDate: new Date(), // Initial start date
+      endDate: new Date(), // Initial end date
+      key: "selection",
+    },
+  ]);
+  const [openDate, setOpenDate] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
+
+  console.log(dates);
 
   return (
     <>
-      <section className="bg-paleblue px-4 py-6 rounded-lg mt-5">
+      <section className="bg-white shadow-xl px-4 py-3 rounded-lg mt-5">
         <div className="container mx-auto flex gap-2 flex-wrap justify-between items-center">
           {/* Search Bar */}
           <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
-              <div className="w-full relative">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-                <input
-                  id="search"
-                  placeholder="Search by city..."
-                  type="text"
-                  className={`
+            <div className="w-full relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <input
+                id="search"
+                placeholder="Destination"
+                type="text"
+                className={`
                       peer
                       w-full
-                      p-4
+                      p-3
                       pt-4
                       pl-10
                       font-light 
@@ -49,12 +64,12 @@ const Search = () => {
                       disabled:opacity-70
                       disabled:cursor-not-allowed
                     `}
-                />
-              </div>
+              />
             </div>
+          </div>
 
           {/* Room Type Select */}
-          <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
+          {/* <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
             <Select
               placeholder="Select Room Types"
               isClearable
@@ -90,51 +105,56 @@ const Search = () => {
                 },
               })}
             />
-          </div>
-          {/* Select City */}
-          {/* <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
-            <Select
-              placeholder="Select City"
-              isClearable
-              options={_city}
-              defaultValue={selectedCity}
-              onChange={setSelectedCity}
-              formatOptionLabel={(option: any) => (
-                <div
-                  className="
-          flex flex-row items-center gap-3"
-                >
-                  <div>
-                    {option.name},
-                    <span className="text-neutral-500 ml-1">
-                      {option.state}
-                    </span>
-                  </div>
-                </div>
-              )}
-              classNames={{
-                control: () => "p-2 border-2",
-                input: () => "text-lg",
-                option: () => "text-lg",
-              }}
-              theme={(theme) => ({
-                ...theme,
-                borderRadius: 6,
-                colors: {
-                  ...theme.colors,
-                  primary: "black",
-                  primary25: "#ffe4e6",
-                },
-              })}
-            />
           </div> */}
 
-          <button
-            className="text-Blueviolet text-lg font-medium py-5 px-16 transition duration-150 ease-in-out rounded-full bg-semiblueviolet hover:text-white hover:bg-Blueviolet"
-            // onClick={() => navigate("/hotels")}
+          {/* Select Date */}
+          <div className="flex items-center gap-4">
+            <Icon icon="mdi:calendar-outline" width={30} height={30} />
+            <span
+              onClick={() => setOpenDate(!openDate)}
+              className="text-lightgray cursor-pointer"
+            >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+              dates[0].endDate,
+              "MM/dd/yyyy"
+            )}`}</span>
+
+            {openDate && (
+              <div
+                className="
+              absolute 
+              rounded-xl 
+              shadow-md
+              w-auto
+              bg-white 
+              overflow-hidden 
+              right-15
+              text-sm
+              top-5
+              mt-[170px]
+            "
+              >
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={(item: any) => setDates([item.selection])}
+                  moveRangeOnFirstSelection={false}
+                  ranges={dates}
+                  minDate={new Date()}
+                />
+              </div>
+            )}
+          </div>
+
+          <div
+            className="
+              p-2 
+              bg-Blueviolet
+              rounded-full 
+              text-white
+              cursor-pointer
+            "
           >
-            Search
-          </button>
+            <Icon icon="material-symbols:search" height={30} width={30} />
+          </div>
         </div>
       </section>
     </>
