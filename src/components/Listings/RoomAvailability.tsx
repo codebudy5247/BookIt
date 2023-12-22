4e9d73927fafd0f918f925ee00e1afd8adf85b82
codebudy5247 <a.shekhar5247@gmail.com>
2023-12-22 15:54:43 +0530

    update style

--- src/components/Listings/RoomAvailability.tsx
+++ src/components/Listings/RoomAvailability.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import { useGetHotelRoomsQuery } from "../../redux/api/hotelApi";
 import Loader from "../Loader";
 import { Icon } from "@iconify/react";
+import Select from "react-select";
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { format } from "date-fns";
+
+const options = [
+  { value: "Basic_room", label: "Basic Room" },
+  { value: "luxury_room", label: "Luxury Room" },
+  { value: "suite", label: "Suite" },
+];
 
 const RoomAvailability = (props: any) => {
   const { data, isError, isLoading } = useGetHotelRoomsQuery(props?.hotelID);
-  
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [dates, setDates] = useState([
+    {
+      startDate: new Date(), // Initial start date
+      endDate: new Date(), // Initial end date
+      key: "selection",
+    },
+  ]);
+  const [openDate, setOpenDate] = useState(false);
+
+  console.log({ openDate, dates });
+
   const renderIcons = (num: any) => {
     const icons = [];
     for (let i = 0; i < num; i++) {
@@ -27,7 +50,65 @@ const RoomAvailability = (props: any) => {
   return (
     <>
       <h2 className="font-bold text-3xl mb-4">Availability</h2>
-      <div className="">
+
+      <div className="flex items-center gap-10">
+        <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
+          <Select
+            placeholder="Select Room Types"
+            isClearable
+            options={options}
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            formatOptionLabel={(option: any) => (
+              <div
+                className="
+          flex flex-row items-center gap-3"
+              >
+                <div>{option.flag}</div>
+                <div>{option.label}</div>
+              </div>
+            )}
+            classNames={{
+              control: () => "p-2 border-2",
+              input: () => "text-lg",
+              option: () => "text-lg",
+            }}
+            theme={(theme) => ({
+              ...theme,
+              borderRadius: 6,
+              colors: {
+                ...theme.colors,
+                primary: "black",
+                primary25: "#ffe4e6",
+              },
+            })}
+          />
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Icon icon="mdi:calendar-outline" width={30} height={30} />
+          <span
+            onClick={() => setOpenDate(!openDate)}
+            className="text-Blueviolet font-bold cursor-pointer"
+          >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+            dates[0].endDate,
+            "MM/dd/yyyy"
+          )}`}</span>
+
+          {openDate && (
+            <DateRange
+              editableDateInputs={true}
+              onChange={(item: any) => setDates([item.selection])}
+              moveRangeOnFirstSelection={false}
+              ranges={dates}
+              minDate={new Date()}
+              className="absolute mt-[430px]"
+            />
+          )}
+        </div>
+      </div>
+
+      <div className="mt-5">
         <table className="w-full border-1 border-lightgray">
           <thead className="bg-semiblueviolet">
             <tr>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,9 @@
 import { useState } from "react";
 import { Icon } from "@iconify/react";
-import Select from "react-select";
-import { DateRange, Range } from "react-date-range";
+import { DateRange } from "react-date-range";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { format } from "date-fns";
-
-const options = [
-  { value: "Basic_room", label: "Basic Room" },
-  { value: "luxury_room", label: "Luxury Room" },
-  { value: "suite", label: "Suite" },
-];
 
 const Search = () => {
   const [dates, setDates] = useState([
@@ -21,7 +14,6 @@ const Search = () => {
     },
   ]);
   const [openDate, setOpenDate] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(null);
 
   console.log(dates);
 
@@ -29,28 +21,29 @@ const Search = () => {
     <>
       <section className="bg-white shadow-xl px-4 py-3 rounded-lg mt-5">
         <div className="container mx-auto flex gap-2 flex-wrap justify-between items-center">
-          {/* Search Bar */}
-          <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
-            <div className="w-full relative">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-              <input
-                id="search"
-                placeholder="Destination"
-                type="text"
-                className={`
+          <div className="flex items-center">
+            {/* Search Bar */}
+            <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
+              <div className="w-full relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+                <input
+                  id="search"
+                  placeholder="Destination"
+                  type="text"
+                  className={`
                       peer
                       w-full
                       p-3
@@ -64,72 +57,32 @@ const Search = () => {
                       disabled:opacity-70
                       disabled:cursor-not-allowed
                     `}
-              />
+                />
+              </div>
+            </div>
+            {/* Select Date */}
+            <div className="flex items-center gap-4">
+              <Icon icon="mdi:calendar-outline" width={30} height={30} />
+              <span
+                onClick={() => setOpenDate(!openDate)}
+                className="text-Blueviolet cursor-pointer"
+              >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
+                dates[0].endDate,
+                "MM/dd/yyyy"
+              )}`}</span>
+
+              {openDate && (
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={(item: any) => setDates([item.selection])}
+                  moveRangeOnFirstSelection={false}
+                  ranges={dates}
+                  minDate={new Date()}
+                  className="absolute top-[180px] z-20"
+                />
+              )}
             </div>
           </div>
-
-          {/* Room Type Select */}
-          {/* <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
-            <Select
-              placeholder="Select Room Types"
-              isClearable
-              options={options}
-              defaultValue={selectedOption}
-              onChange={setSelectedOption}
-              formatOptionLabel={(option: any) => (
-                <div
-                  className="
-          flex flex-row items-center gap-3"
-                >
-                  <div>{option.flag}</div>
-                  <div>
-                    {option.label},
-                    <span className="text-neutral-500 ml-1">
-                      {option.region}
-                    </span>
-                  </div>
-                </div>
-              )}
-              classNames={{
-                control: () => "p-2 border-2",
-                input: () => "text-lg",
-                option: () => "text-lg",
-              }}
-              theme={(theme) => ({
-                ...theme,
-                borderRadius: 6,
-                colors: {
-                  ...theme.colors,
-                  primary: "black",
-                  primary25: "#ffe4e6",
-                },
-              })}
-            />
-          </div> */}
-
-          {/* Select Date */}
-          <div className="flex items-center gap-4">
-            <Icon icon="mdi:calendar-outline" width={30} height={30} />
-            <span
-              onClick={() => setOpenDate(!openDate)}
-              className="text-lightgray cursor-pointer"
-            >{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(
-              dates[0].endDate,
-              "MM/dd/yyyy"
-            )}`}</span>
-
-            {openDate && (
-              <DateRange
-                editableDateInputs={true}
-                onChange={(item: any) => setDates([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={dates}
-                minDate={new Date()}
-                className="absolute top-[180px] z-20"
-              />
-            )}
-          </div>
-
           <div
             className="
               p-2 
