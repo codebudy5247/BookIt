f317c12792d67ec104599a43777e2a29bbf900bf
codebudy5247 <a.shekhar5247@gmail.com>
2023-12-23 14:47:27 +0530

    code refactor

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,14 @@
+import { useGetHotelsQuery } from "../redux/api/hotelApi";
 import HotelListing from "../components/Listings/HotelListing";
 import Container from "../components/Container";
 import Search from "../components/Search";
 const Home = () => {
+  const { data, isLoading, isError } = useGetHotelsQuery();
   return (
     <div>
       <Container>
         <Search />
-        <HotelListing />
+        <HotelListing hotels={data!} isLoading={isLoading} isError={isError} />
       </Container>
     </div>
   );
--- src/components/Listings/HotelListing.tsx
+++ src/components/Listings/HotelListing.tsx
@@ -1,22 +1,25 @@
-import { useGetHotelsQuery } from "../../redux/api/hotelApi";
+import { HotelResponse } from "../../types/hotel";
 import Loader from "../Loader";
 import ListingCard from "./ListingCard";
 
-const HotelList = () => {
-  const { data, isLoading, isError } = useGetHotelsQuery();
-
-  if (isError)
+interface propTypes {
+  hotels: HotelResponse[];
+  isLoading: boolean;
+  isError: boolean;
+}
+const HotelList = (props: propTypes) => {
+  if (props.isError)
     return (
       <h1 className="mt-10 font-bold text-5xl text-center text-Blueviolet bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         Something went wrong.Try after sometime!
       </h1>
     );
-  if (isLoading) return <Loader />;
+  if (props.isLoading) return <Loader />;
 
   return (
     <div className="grid gap-x-6 gap-y-8 grid-cols-1 md:grid-cols-4 lg:grid-cols-4 mt-5">
-      {data?.length! > 0 &&
-        data?.map((hotel) => (
+      {props?.hotels?.length! > 0 &&
+        props?.hotels?.map((hotel) => (
           <div key={hotel._id}>
             <ListingCard data={hotel} />
           </div>
