6e5eaafb63bc70b786cc4971312314c513678d09
codebudy5247 <a.shekhar5247@gmail.com>
2023-12-26 17:01:22 +0530

    fixed bug

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -22,6 +22,28 @@ const Search = () => {
   const [openDate, setOpenDate] = useState(false);
   const [destination, setDestination] = useState();
 
+  const handleOnChangeDestination = (e: any) => {
+    setDestination(e.target.value);
+    dispatch(
+      SearchData({
+        destination,
+        checkInDate: dates[0].startDate,
+        checkOutDate: dates[0].endDate,
+      })
+    );
+  };
+
+  const handleOnChangeDates = (item: any) => {
+    setDates([item.selection]);
+    dispatch(
+      SearchData({
+        destination,
+        checkInDate: dates[0].startDate,
+        checkOutDate: dates[0].endDate,
+      })
+    );
+  };
+
   const handleClickEvent = () => {
     navigate("/hotels");
     dispatch(
@@ -37,31 +59,31 @@ const Search = () => {
     <>
       <section className="bg-white shadow-xl px-4 py-3 rounded-lg mt-5">
         <div className="container mx-auto flex items-center justify-between">
-         <div className="flex items-center gap-20">
-           {/* Search Bar */}
-           <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
-            <div className="w-full relative">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-              <input
-                id="search"
-                onChange={(e: any) => setDestination(e.target.value)}
-                placeholder="Destination"
-                type="text"
-                defaultValue={searchDetails.destination}
-                className={`
+          <div className="flex items-center gap-20">
+            {/* Search Bar */}
+            <div className="w-full md:1/3 lg:w-auto mb-4 md:mb-0">
+              <div className="w-full relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+                <input
+                  id="search"
+                  onChange={handleOnChangeDestination}
+                  placeholder="Destination"
+                  type="text"
+                  defaultValue={searchDetails.destination}
+                  className={`
                       peer
                       w-full
                       p-3
@@ -75,32 +97,32 @@ const Search = () => {
                       disabled:opacity-70
                       disabled:cursor-not-allowed
                     `}
-              />
+                />
+              </div>
+            </div>
+            {/* Select Date */}
+            <div className="flex items-center gap-4">
+              <Icon icon="mdi:calendar-outline" width={30} height={30} />
+              <span
+                onClick={() => setOpenDate(!openDate)}
+                className="text-Blueviolet cursor-pointer"
+              >{`${format(searchDetails.checkInDate, "MM/dd/yyyy")} to ${format(
+                searchDetails.checkOutDate,
+                "MM/dd/yyyy"
+              )}`}</span>
+
+              {openDate && (
+                <DateRange
+                  editableDateInputs={true}
+                  onChange={handleOnChangeDates}
+                  moveRangeOnFirstSelection={false}
+                  ranges={dates}
+                  minDate={new Date()}
+                  className="absolute top-[180px] z-20"
+                />
+              )}
             </div>
           </div>
-          {/* Select Date */}
-          <div className="flex items-center gap-4">
-            <Icon icon="mdi:calendar-outline" width={30} height={30} />
-            <span
-              onClick={() => setOpenDate(!openDate)}
-              className="text-Blueviolet cursor-pointer"
-            >{`${format(searchDetails.checkInDate, "MM/dd/yyyy")} to ${format(
-              searchDetails.checkOutDate,
-              "MM/dd/yyyy"
-            )}`}</span>
-
-            {openDate && (
-              <DateRange
-                editableDateInputs={true}
-                onChange={(item: any) => setDates([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={dates}
-                minDate={new Date()}
-                className="absolute top-[180px] z-20"
-              />
-            )}
-          </div>
-         </div>
 
           {location.pathname === "/" && (
             <div
