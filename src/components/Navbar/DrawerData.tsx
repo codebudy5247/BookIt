f5357b25e6879ae8cd9bab532e135b103d6afa05
codebudy5247 <a.shekhar5247@gmail.com>
2024-01-02 18:14:10 +0530

    update navbar drawer

--- src/components/Navbar/DrawerData.tsx
+++ src/components/Navbar/DrawerData.tsx
@@ -1,51 +1,42 @@
-import { Link } from "react-router-dom";
-
-// interface NavigationItem {
-//   name: string;
-//   href: string;
-//   current: boolean;
-// }
-
-// const navigation: NavigationItem[] = [
-//   { name: "Home", href: "/", current: true },
-//   { name: "Courses", href: "#courses", current: false },
-//   { name: "Mentor", href: "#mentor", current: false },
-//   { name: "Group", href: "#/", current: false },
-//   { name: "Testimonial", href: "#testimonial", current: false },
-// ];
-
-// function classNames(...classes: string[]) {
-//   return classes.filter(Boolean).join(" ");
-// }
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
 const Data = () => {
+  const navigate = useNavigate();
+  const [cookies] = useCookies(["logged_in"]);
+  const logged_in = cookies.logged_in;
   return (
     <div className="rounded-md max-w-sm w-full mx-auto">
       <div className="flex-1 space-y-4 py-1">
         <div className="sm:block">
           <div className="space-y-1 px-5 pt-2 pb-3">
-            {/* {navigation.map((item) => (
-              <Link
-                key={item.name}
-                to={item.href}
-                className={classNames(
-                  item.current
-                    ? "text-black hover:opacity-100"
-                    : "hover:text-black hover:opacity-100",
-                  "py-1 text-lg font-normal opacity-75 block"
-                )}
-                aria-current={item.current ? "page" : undefined}
-              >
-                {item.name}
-              </Link>
-            ))} */}
             <div className="mt-4"></div>
-            <button className="bg-white w-full text-Blueviolet border border-semiblueviolet font-medium py-2 px-4 rounded">
-              Log In
-            </button>
-            <button className="bg-semiblueviolet w-full hover:bg-Blueviolet hover:text-white text-Blueviolet font-medium my-2 py-2 px-4 rounded">
-              Sign up
-            </button>
+            {!logged_in && (
+              <>
+                <button
+                  onClick={() => navigate("/login")}
+                  className="bg-white w-full text-Blueviolet border border-semiblueviolet font-medium py-2 px-4 rounded"
+                >
+                  Log In
+                </button>
+                <button
+                  onClick={() => navigate("/register")}
+                  className="bg-semiblueviolet w-full hover:bg-Blueviolet hover:text-white text-Blueviolet font-medium my-2 py-2 px-4 rounded"
+                >
+                  Sign up
+                </button>
+              </>
+            )}
+
+            {logged_in && (
+                
+                <button
+                  className="bg-semiblueviolet w-full hover:bg-Blueviolet hover:text-white text-Blueviolet font-medium my-2 py-2 px-4 rounded"
+                >
+                  Logout
+                </button>
+            )}
           </div>
         </div>
       </div>
