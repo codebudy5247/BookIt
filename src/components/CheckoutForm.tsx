84e5be27b8cdba0c93226bd1659ade79e07d2a35
codebudy5247 <a.shekhar5247@gmail.com>
2024-01-03 14:55:18 +0530

    stripe payment integration

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -2,6 +2,9 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { StripeCardElement } from "@stripe/stripe-js";
 import { IUser } from "../types/user";
 import { Ibooking } from "../types/booking";
+import { toast } from "react-toastify";
+import { PuffLoader } from "react-spinners";
+import { useState } from "react";
 
 type Props = {
   currentUser: IUser;
@@ -11,11 +14,32 @@ type Props = {
 const CheckoutForm = ({ currentUser, paymentIntent }: Props) => {
   const stripe = useStripe();
   const elements = useElements();
+
+  const [isLoading, setIsLoading] = useState<boolean>();
+
+  const onSubmit = async () => {
+    if (!stripe || !elements) {
+      return;
+    }
+    setIsLoading(true);
+    const result = await stripe.confirmCardPayment(
+      paymentIntent?.payment?.clientSecret,
+      {
+        payment_method: {
+          card: elements.getElement(CardElement) as StripeCardElement,
+        },
+      }
+    );
+    if (result?.error) {
+      toast.error(result.error?.message);
+    }
+    if (result?.paymentIntent?.status === "succeeded") {
+      toast.success("Payment Successfull");
+    }
+    setIsLoading(false);
+  };
   return (
-    <form
-      // onSubmit={handleSubmit(onSubmit)}
-      className="grid grid-cols-1 gap-5 rounded-lg shadow-2xl p-5"
-    >
+    <div className="grid grid-cols-1 gap-5 rounded-lg shadow-2xl p-5">
       <span className="text-3xl font-bold">Confirm Your Details</span>
       <div className="grid md:grid-cols-[3fr_3fr] gap-4">
         <input
@@ -23,7 +47,7 @@ const CheckoutForm = ({ currentUser, paymentIntent }: Props) => {
           placeholder="Destination"
           type="text"
           disabled
-          defaultValue={currentUser.name}
+          defaultValue={currentUser?.name}
           className={`
                       peer
                       w-full
@@ -42,7 +66,7 @@ const CheckoutForm = ({ currentUser, paymentIntent }: Props) => {
           placeholder="Destination"
           type="text"
           disabled
-          defaultValue={currentUser.email}
+          defaultValue={currentUser?.email}
           className={`
                       peer
                       w-full
@@ -78,15 +102,18 @@ const CheckoutForm = ({ currentUser, paymentIntent }: Props) => {
 
       <div className="flex justify-end">
         <button
-          //   disabled
-          type="submit"
+          disabled={isLoading}
+          onClick={onSubmit}
           className="bg-semiblueviolet text-Blueviolet p-2 font-bold hover:text-white hover:bg-Blueviolet text-md disabled:bg-grey500 disabled:text-lightgray"
         >
-          {/* {isLoading ? "Saving..." : "Confirm Booking"} */}
-          Confirm Booking
+          {isLoading ? (
+            <PuffLoader size={50} color="blue" />
+          ) : (
+            "Confirm Booking"
+          )}
         </button>
       </div>
-    </form>
+    </div>
   );
 };
 
